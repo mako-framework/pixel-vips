4945ee3dd658b6301d594bb05e4e369af4e3eb02
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-27 22:04:54 +0200

    Add Invert operation

---
+++ src/operations/vips/Invert.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @copyright Frederic G. Østby
+ * @license   http://www.makoframework.com/license
+ */
+
+namespace mako\pixel\image\operations\vips;
+
+use Jcupitt\Vips\Image;
+use mako\pixel\image\operations\OperationInterface;
+use Override;
+
+/**
+ * Inverts the colors of the image.
+ */
+class Invert implements OperationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param Image &$imageResource
+	 */
+	#[Override]
+	public function apply(object &$imageResource): void
+	{
+		$alpha = null;
+
+		if ($imageResource->hasAlpha()) {
+			$alpha = $imageResource->extract_band($imageResource->bands - 1);
+
+			$imageResource = $imageResource->extract_band(0, ['n' => $imageResource->bands - 1]);
+		}
+
+		$imageResource = $imageResource->invert();
+
+		if ($alpha !== null) {
+			$imageResource = $imageResource->bandjoin($alpha);
+		}
+	}
+}
