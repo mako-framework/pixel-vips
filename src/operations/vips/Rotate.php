c7daed5a14cc2422cea1c040e7c6bce456e2ef10
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-27 22:30:29 +0200

    Add Rotate operation

---
+++ src/operations/vips/Rotate.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @copyright Frederic G. Østby
+ * @license   http://www.makoframework.com/license
+ */
+
+namespace mako\pixel\image\operations\vips;
+
+use Jcupitt\Vips\Image;
+use mako\pixel\image\operations\OperationInterface;
+use Override;
+
+/**
+ * Rotates the image.
+ */
+class Rotate implements OperationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function __construct(
+		protected int $degrees = 0
+	) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param Image &$imageResource
+	 */
+	#[Override]
+	public function apply(object &$imageResource): void
+	{
+		$degrees = (($this->degrees % 360) + 360) % 360;
+
+		if ($degrees === 0) {
+			return;
+		}
+
+		// Use the fast lossless rotations for right angles
+
+		if ($degrees % 90 === 0) {
+			$imageResource = match ($degrees) {
+				90  => $imageResource->rot90(),
+				180 => $imageResource->rot180(),
+				270 => $imageResource->rot270(),
+			};
+
+			return;
+		}
+
+		// Arbitrary angles require interpolation and a transparent background
+
+		if (!$imageResource->hasAlpha()) {
+			$imageResource = $imageResource->bandjoin_const(255);
+		}
+
+		$imageResource = $imageResource->similarity([
+			'angle' => $degrees,
+			'background' => [0, 0, 0, 0],
+		]);
+	}
+}
