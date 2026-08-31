3bcdf40b516994f69ad49a8dc19edaa3abb8e03b
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-31 16:47:27 +0200

    More tests

--- tests/unit/pixel/image/operations/vips/BorderTest.php
+++ tests/unit/pixel/image/operations/vips/BorderTest.php
@@ -9,7 +9,11 @@ namespace mako\tests\unit\pixel\image\operations\vips;
 
 use InvalidArgumentException;
 use mako\pixel\image\Color;
+use mako\pixel\image\geometry\Dimensions;
+use mako\pixel\image\geometry\Point;
+use mako\pixel\image\inspectors\vips\ColorAt;
 use mako\pixel\image\operations\vips\Border;
+use mako\pixel\image\Vips;
 use mako\tests\TestCase;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
@@ -27,5 +31,38 @@ class BorderTest extends TestCase
 		$this->expectExceptionMessageIs('The border width must be a non-negative number.');
 
 		new Border(new Color(0, 0, 0), -4);
+	}
+
+	/**
+	 *
+	 */
+	public function testBorder(): void
+	{
+		$image = Vips::create(new Dimensions(10, 10), new Color(0, 0, 0));
+
+		// Check that the borders are black
+
+		$color = $image->inspect(new ColorAt(new Point(2, 0)));
+
+		$this->assertSame('#000000', $color->toHexString());
+
+		// Draw border
+
+		$image->apply(new Border(new Color(255, 0, 0), 2));
+
+		// Top, bottom, left and right edges should be red
+
+		$this->assertSame('#FF0000', $image->inspect(new ColorAt(new Point(2, 0)))->toHexString());
+		$this->assertSame('#FF0000', $image->inspect(new ColorAt(new Point(5, 9)))->toHexString());
+		$this->assertSame('#FF0000', $image->inspect(new ColorAt(new Point(0, 5)))->toHexString());
+		$this->assertSame('#FF0000', $image->inspect(new ColorAt(new Point(9, 5)))->toHexString());
+
+		// First pixel inside the border should be unchanged
+
+		$this->assertSame('#000000', $image->inspect(new ColorAt(new Point(2, 2)))->toHexString());
+
+		// Center should be unchanged
+
+		$this->assertSame('#000000', $image->inspect(new ColorAt(new Point(5, 5)))->toHexString());
 	}
 }
